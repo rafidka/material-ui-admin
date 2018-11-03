2a7ae3345ac194811a466109ffa82ed6889e5f77
Rafid K. Al-Humaimidi <rafidka@gmail.com>
2018-11-02 17:59:00 -0700

    Set viewport and add CssBaseline

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
 import Button from "@material-ui/core/Button";
+import { CssBaseline } from "@material-ui/core";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
+        <CssBaseline />
         <Button variant="contained" color="primary">
           Hello World
         </Button>
-      </div>
+      </React.Fragment>
     );
   }
 }
